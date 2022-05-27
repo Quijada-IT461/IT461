b9935421e064c67fe5f40b97cb9fb5411e6d349d
Orlan Quijada <orlantquijada@gmail.com>
2022-05-27 14:56:42 +0800

    feat: delete dog

--- client/src/components/Dogs.js
+++ client/src/components/Dogs.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect, useCallback, useRef, forwardRef } from 'react';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const Dogs = () => {
     const [dogs, setDogs] = useState();
@@ -8,25 +8,41 @@ const Dogs = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const getDogs = async (url, options) => {
-        try {
-            const response = await axiosPrivate.get(url, options);
-            console.log(response.data);
-            setDogs(response.data);
-        } catch (err) {
-            console.error(err);
-            navigate('/login', { state: { from: location }, replace: true });
-        }
-    }
+    const getDogs = useCallback(
+        async (url, options) => {
+            try {
+                const response = await axiosPrivate.get(url, options);
+                console.log(response.data);
+                setDogs(response.data);
+            } catch (err) {
+                console.error(err);
+                navigate('/login', {
+                    state: { from: location },
+                    replace: true,
+                });
+            }
+        },
+        [axiosPrivate, location, navigate]
+    );
+
+    const deleteDog = useCallback(
+        (id) =>
+            axiosPrivate.delete(`/dogs/${id}`).then(() => {
+                // refetch on success
+                getDogs();
+            }),
+        [axiosPrivate, getDogs]
+    );
+
     useEffect(() => {
         const controller = new AbortController();
         getDogs('/dogs/?limit=3&offset=0', {
-            signal: controller.signal
+            signal: controller.signal,
         });
         return () => {
             controller.abort();
-        }
-    }, []);
+        };
+    }, [getDogs]);
 
     const paginationHandler = (e) => {
         e.preventDefault();
@@ -35,13 +51,16 @@ const Dogs = () => {
             const url = dogs.metadata.links[name];
             getDogs(url);
         }
-    }
+    };
+
+    const dialogRef = useRef();
+
     return (
         <article>
             <h2>Dogs List</h2>
-            {dogs?.data?.length
-                ? (
-                    <>
+
+            {dogs?.data?.length ? (
+                <>
                     <table border="1" cellpading="5" cellspacing="5">
                         <thead>
                             <tr>
@@ -51,42 +70,86 @@ const Dogs = () => {
                             </tr>
                         </thead>
                         <tbody>
-                    {
-                        dogs.data.map((dog, i) =>
-                            <tr key={dog.id}>
-                                <td>{dog.id}</td>
-                                <td>{dog.name}</td>
-                                <td>
-                                    <a href=""> View </a> |
-                                    <a href=""> Edit </a> |
-                                    <a href=""> Delete </a>
-                                </td>
-                            </tr>
-                        )
-                    }
+                            {dogs.data.map((dog, i) => (
+                                <tr key={dog.id}>
+                                    <td>{dog.id}</td>
+                                    <td>{dog.name}</td>
+                                    <td>
+                                        <a href=""> View </a> |
+                                        <a href=""> Edit </a> |
+                                        <a
+                                            href=""
+                                            onClick={(e) => {
+                                                e.preventDefault();
+                                                dialogRef.current.showModal();
+                                            }}
+                                        >
+                                            Delete
+                                        </a>
+                                        <DeleteDialogForm
+                                            ref={dialogRef}
+                                            onSubmit={() => {
+                                                deleteDog(dog.id);
+                                            }}
+                                        />
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
-                    {dogs?.metadata?.links?.previous ? 
+                    {dogs?.metadata?.links?.previous ? (
                         <a
                             href="#"
                             data-name="previous"
                             onClick={paginationHandler}
-                        > &lsaquo;Previous </a>
-                        : ''
-                    }
-                    {dogs?.metadata?.links?.next ? 
+                        >
+                            {' '}
+                            &lsaquo;Previous{' '}
+                        </a>
+                    ) : (
+                        ''
+                    )}
+                    {dogs?.metadata?.links?.next ? (
                         <a
                             href="#"
                             data-name="next"
                             onClick={paginationHandler}
-                        > Next&rsaquo; </a>
-                        : ''
-                    }
-                    </>
-                ) : <p>No dogs to display</p>
-            }
+                        >
+                            {' '}
+                            Next&rsaquo;{' '}
+                        </a>
+                    ) : (
+                        ''
+                    )}
+                </>
+            ) : (
+                <p>No dogs to display</p>
+            )}
         </article>
     );
 };
+
+const DeleteDialogForm = forwardRef((props, ref) => (
+    <dialog ref={ref}>
+        <form method="dialog" onSubmit={props.onSubmit}>
+            <header>
+                <h4>Are you sure?</h4>
+                <p>You cannot undo this action.</p>
+            </header>
+            <footer>
+                <menu>
+                    <button
+                        className="cancel"
+                        type="button"
+                        onClick={() => ref.current.close()}
+                    >
+                        Cancel
+                    </button>
+                    <button>Confirm</button>
+                </menu>
+            </footer>
+        </form>
+    </dialog>
+));
 
 export default Dogs;
